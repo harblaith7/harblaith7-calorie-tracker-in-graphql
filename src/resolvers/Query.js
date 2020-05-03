2100893803f88efacd7ea4424e70ba2785c58f65
Laith Harb <harblaith77@gmail.com>
2020-05-03 11:06:48 -0400

    added args to query schema and refactored query resolver

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,10 +3,27 @@
 const Query = {
 
     user(parent, args, {db}, info) {
+
+        if(args.id){
+            const userExists = db.users.some(user => {
+                return user.id === args.id
+            })
+
+            if(!userExists){
+                throw new Error("No user with such ID")
+            }
+
+            return db.users.filter(user => {
+                return user.id === args.id
+            })
+        }
         return db.users
     }
-
 }
+
+
+
+
 
 
 
